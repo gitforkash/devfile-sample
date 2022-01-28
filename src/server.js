f575adbccfe2e29293dd2525f318317397cd9b81
gitforkash <kashyap.m@gmail.com>
2022-01-28 09:09:33 -0800

    Changed server to redirect to http for https

--- src/server.js
+++ src/server.js
@@ -50,6 +50,9 @@ const pino = require('pino')({
 app.use(require('pino-http')({logger: pino}));
 
 app.get('/', (req, res) => {	
+    if (req.protocol == 'https'){
+        res.redirect('http://'+req.get('host')+req.originalUrl);
+    }
   // Use req.log (a `pino` instance) to log JSON:	
   req.log.info({message: 'Hello from Node.js Starter Application from Cluster2!'});		
   res.send('Hello from Node.js Starter Application from Cluster2!');	
